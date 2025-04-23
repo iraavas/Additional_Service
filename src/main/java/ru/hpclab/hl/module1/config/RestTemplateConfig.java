bed73eb4857629b63a4126986a55517ae90860ee
Irina <iravas981@icloud.com>
2025-04-23 23:51:15 +0300

    LAB

--- src/main/java/ru/hpclab/hl/module1/config/RestTemplateConfig.java
+++ src/main/java/ru/hpclab/hl/module1/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package ru.hpclab.hl.module1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,10 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(120000); // таймаут установки соединения: 60 секунд
+        factory.setReadTimeout(120000);    // таймаут ожидания ответа: 60 секунд
+
+        return new RestTemplate(factory);
     }
 }
